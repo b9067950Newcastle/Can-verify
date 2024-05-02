a6bb7ffe58da2a918c5359c5c62b2676d78d109a
b9067950Newcastle <91704656+b9067950Newcastle@users.noreply.github.com>
2024-05-02 16:17:51 +0100

    update new rule via upload

--- PropertiesCompiler/compile.ml
+++ PropertiesCompiler/compile.ml
@@ -1,14 +1,14 @@
 open Syntax
 
 let preds = ref [||]
-let c_quantity q = match q with All -> "A" | Som -> "E"
+let c_quantity q = match q with All -> "Pmin" | Som -> "Pmax"
 
 let fold_bel str ch =
   if String.equal str "" then Printf.sprintf "predicate_%s" ch
   else Printf.sprintf "%s_%s" str ch
 
 let c_belief bel = Array.fold_left fold_bel "" bel
-let build_prop q beliefs = Printf.sprintf "%s [F (\"%s\")];" q beliefs
+let build_prop q beliefs = Printf.sprintf "%s =? [F (\"%s\")];" q beliefs
 
 let rec c_line line =
   match line with
